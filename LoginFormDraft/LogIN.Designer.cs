80bcc5bc5f0e6de5570dd2530bb28899da524912
SirOmar <SirOmar@LAPTOP-10B1POUQ>
2025-04-03 20:11:01 +0800

    Add project files.

---
+++ LoginFormDraft/LogIN.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace LoginFormDraft
+{
+    partial class LogIN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelNavy = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSystem = new System.Windows.Forms.Label();
+            this.lblFoodie = new System.Windows.Forms.Label();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.PicLogo = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ForgotPassbtn = new System.Windows.Forms.Button();
+            this.loginbtn = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.PicLogin = new System.Windows.Forms.PictureBox();
+            this.btnX = new System.Windows.Forms.Button();
+            this.PanelNavy.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicLogo)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicLogin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PanelNavy
+            // 
+            this.PanelNavy.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(52)))), ((int)(((byte)(77)))));
+            this.PanelNavy.Controls.Add(this.label2);
+            this.PanelNavy.Controls.Add(this.label1);
+            this.PanelNavy.Controls.Add(this.lblSystem);
+            this.PanelNavy.Controls.Add(this.lblFoodie);
+            this.PanelNavy.Controls.Add(this.lblWelcome);
+            this.PanelNavy.Controls.Add(this.PicLogo);
+            this.PanelNavy.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PanelNavy.Location = new System.Drawing.Point(0, 0);
+            this.PanelNavy.Name = "PanelNavy";
+            this.PanelNavy.Size = new System.Drawing.Size(300, 577);
+            this.PanelNavy.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(6, 556);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Group 36";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(6, 539);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Developed by";
+            // 
+            // lblSystem
+            // 
+            this.lblSystem.AutoSize = true;
+            this.lblSystem.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSystem.ForeColor = System.Drawing.Color.White;
+            this.lblSystem.Location = new System.Drawing.Point(3, 240);
+            this.lblSystem.Name = "lblSystem";
+            this.lblSystem.Size = new System.Drawing.Size(109, 34);
+            this.lblSystem.TabIndex = 3;
+            this.lblSystem.Text = "System";
+            // 
+            // lblFoodie
+            // 
+            this.lblFoodie.AutoSize = true;
+            this.lblFoodie.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFoodie.ForeColor = System.Drawing.Color.White;
+            this.lblFoodie.Location = new System.Drawing.Point(3, 206);
+            this.lblFoodie.Name = "lblFoodie";
+            this.lblFoodie.Size = new System.Drawing.Size(186, 34);
+            this.lblFoodie.TabIndex = 2;
+            this.lblFoodie.Text = "FoodiePoint ";
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.ForeColor = System.Drawing.Color.White;
+            this.lblWelcome.Location = new System.Drawing.Point(3, 170);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(235, 34);
+            this.lblWelcome.TabIndex = 1;
+            this.lblWelcome.Text = "Welcome to the";
+            this.lblWelcome.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // PicLogo
+            // 
+            this.PicLogo.Image = global::LoginFormDraft.Properties.Resources.FoodiePoint_removebg_previewww;
+            this.PicLogo.Location = new System.Drawing.Point(68, 12);
+            this.PicLogo.Name = "PicLogo";
+            this.PicLogo.Size = new System.Drawing.Size(147, 118);
+            this.PicLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PicLogo.TabIndex = 0;
+            this.PicLogo.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.ForgotPassbtn);
+            this.panel2.Controls.Add(this.loginbtn);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Controls.Add(this.btnX);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(300, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(464, 577);
+            this.panel2.TabIndex = 1;
+            // 
+            // ForgotPassbtn
+            // 
+            this.ForgotPassbtn.BackColor = System.Drawing.SystemColors.Control;
+            this.ForgotPassbtn.FlatAppearance.BorderSize = 0;
+            this.ForgotPassbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ForgotPassbtn.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForgotPassbtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(52)))), ((int)(((byte)(77)))));
+            this.ForgotPassbtn.Location = new System.Drawing.Point(215, 352);
+            this.ForgotPassbtn.Name = "ForgotPassbtn";
+            this.ForgotPassbtn.Size = new System.Drawing.Size(148, 35);
+            this.ForgotPassbtn.TabIndex = 6;
+            this.ForgotPassbtn.Text = "Forgot Password";
+            this.ForgotPassbtn.UseVisualStyleBackColor = false;
+            // 
+            // loginbtn
+            // 
+            this.loginbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(52)))), ((int)(((byte)(77)))));
+            this.loginbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loginbtn.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginbtn.ForeColor = System.Drawing.Color.White;
+            this.loginbtn.Location = new System.Drawing.Point(34, 352);
+            this.loginbtn.Name = "loginbtn";
+            this.loginbtn.Size = new System.Drawing.Size(148, 35);
+            this.loginbtn.TabIndex = 5;
+            this.loginbtn.Text = "LOGIN";
+            this.loginbtn.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.Control;
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Controls.Add(this.txtPass);
+            this.panel3.Location = new System.Drawing.Point(0, 258);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(464, 45);
+            this.panel3.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LoginFormDraft.Properties.Resources.FoodiePoint__2__removebg_preview;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 28);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // txtPass
+            // 
+            this.txtPass.BackColor = System.Drawing.SystemColors.Control;
+            this.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPass.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(52)))), ((int)(((byte)(77)))));
+            this.txtPass.Location = new System.Drawing.Point(44, 15);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(319, 15);
+            this.txtPass.TabIndex = 3;
+            this.txtPass.UseSystemPasswordChar = true;
+            this.txtPass.Click += new System.EventHandler(this.txtPass_Click);
+            this.txtPass.TextChanged += new System.EventHandler(this.txtPass_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(52)))), ((int)(((byte)(77)))));
+            this.label3.Location = new System.Drawing.Point(51, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 34);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Login";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.PicLogin);
+            this.panel1.Location = new System.Drawing.Point(0, 207);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(464, 45);
+            this.panel1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(52)))), ((int)(((byte)(77)))));
+            this.textBox1.Location = new System.Drawing.Point(44, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(319, 15);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // PicLogin
+            // 
+            this.PicLogin.Image = global::LoginFormDraft.Properties.Resources.profile_circle_256x256;
+            this.PicLogin.Location = new System.Drawing.Point(6, 7);
+            this.PicLogin.Name = "PicLogin";
+            this.PicLogin.Size = new System.Drawing.Size(32, 28);
+            this.PicLogin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PicLogin.TabIndex = 1;
+            this.PicLogin.TabStop = false;
+            // 
+            // btnX
+            // 
+            this.btnX.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnX.FlatAppearance.BorderSize = 0;
+            this.btnX.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnX.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnX.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.btnX.Location = new System.Drawing.Point(424, 0);
+            this.btnX.Name = "btnX";
+            this.btnX.Size = new System.Drawing.Size(40, 40);
+            this.btnX.TabIndex = 0;
+            this.btnX.Text = "X";
+            this.btnX.UseVisualStyleBackColor = true;
+            this.btnX.Click += new System.EventHandler(this.btnX_Click);
+            // 
+            // LogIN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 577);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.PanelNavy);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LogIN";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.PanelNavy.ResumeLayout(false);
+            this.PanelNavy.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicLogo)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicLogin)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelNavy;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.PictureBox PicLogo;
+        private System.Windows.Forms.Label lblSystem;
+        private System.Windows.Forms.Label lblFoodie;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox PicLogin;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button loginbtn;
+        private System.Windows.Forms.Button ForgotPassbtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ LoginFormDraft/LogIN.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing; 
+
+namespace LoginFormDraft
+{
+    public partial class LogIN : Form
+    {
+        public LogIN()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.White;
+            panel1.BackColor = Color.White;
+            panel3.BackColor = SystemColors.Control;
+            txtPass.BackColor = SystemColors.Control;
+
+        }
+
+        private void txtPass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPass_Click(object sender, EventArgs e)
+        {
+            txtPass.BackColor = Color.White;
+            panel3.BackColor = Color.White;
+            textBox1 .BackColor = SystemColors.Control;
+            panel1.BackColor = SystemColors.Control;
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            txtPass.UseSystemPasswordChar = false;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            txtPass.UseSystemPasswordChar= true;
+        }
+    }
+}
